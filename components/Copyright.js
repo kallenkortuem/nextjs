65bb7e7fa8620ef2ca0fe1b10821e5a64e318e3c
Kallen Kortuem <kallenjkortuem@gmail.com>
2021-08-15 23:30:25 -0500

    add static props

--- components/Copyright.js
+++ components/Copyright.js
@@ -5,12 +5,13 @@ import MuiLink from '@material-ui/core/Link';
 export default function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <MuiLink color="inherit" href="https://material-ui.com/">
-        Your Website
-      </MuiLink>{' '}
-      {new Date().getFullYear()}
-      {'.'}
+      <Typography variant="caption">
+        © Copyright {new Date().getFullYear()} fiddlestats.com. Fiddlestats isn&apos;t endorsed by
+        Riot Games and doesn&apos;t reflect the views or opinions of Riot Games
+        or anyone officially involved in producing or managing Riot Games
+        properties. Riot Games, and all associated properties are
+        trademarks or registered trademarks of Riot Games, Inc.
+      </Typography>
     </Typography>
   );
 }
--- pages/mastery/[...summonerNames].tsx
+++ pages/mastery/[...summonerNames].tsx
@@ -1,32 +1,122 @@
-import {  } from "@kallenkortuem/bookish-invention";
-import { Box, Container, Link, Typography } from "@material-ui/core";
+import {
+  ChampionMasteryV4Api,
+  ChampionMasteryV4ChampionMasteryDto,
+  Configuration,
+  SummonerV4Api,
+  SummonerV4SummonerDTO,
+} from "@kallenkortuem/bookish-invention";
+import {
+  Box,
+  Card,
+  CardContent,
+  CardHeader,
+  Container,
+  Grid,
+  LinearProgress,
+  Link,
+  Typography,
+} from "@material-ui/core";
+import {
+  DataGrid,
+  GridColDef,
+  GridValueFormatterParams,
+} from "@material-ui/data-grid";
+import { formatDistanceToNowStrict } from "date-fns";
 import { GetStaticPaths, GetStaticProps } from "next";
 import { useRouter } from "next/router";
 import React from "react";
 import Copyright from "../../components/Copyright";
-import ProTip from "../../components/ProTip";
+
+type NarrowedGridColDef = Omit<GridColDef, "field"> & {
+  field: keyof ChampionMasteryV4ChampionMasteryDto;
+};
 
 interface MasterySummonerNamesProps {
-  
+  data?:
+    | {
+        summoner: SummonerV4SummonerDTO;
+        masteries: ChampionMasteryV4ChampionMasteryDto[];
+      }[]
+    | null;
 }
 
-const MasterySummonerNames = ({}: MasterySummonerNamesProps) => {
-  const router = useRouter();
-  const { summonerNames } = router.query;
+const MasterySummonerNames = ({ data }: MasterySummonerNamesProps) => {
+  const {
+    query: { summonerNames },
+    isFallback,
+  } = useRouter();
+
+  const columns: NarrowedGridColDef[] = [
+    {
+      field: "championId",
+      headerName: "champion",
+      // eslint-disable-next-line react/display-name
+      renderCell: ({ row }) => (
+        <Link variant="body2" underline="hover" color="textPrimary">
+          {row?.championId}
+        </Link>
+      ),
+    },
+    { field: "championLevel", headerName: "masteryLevel"},
+    {
+      field: "championPoints",
+      headerName: "totalPoints",
+      valueFormatter: ({ value }: GridValueFormatterParams) =>
+        value?.toLocaleString(),
+    },
+    {
+      field: "lastPlayTime",
+      headerName: "lastPlayTime",
+      valueFormatter: ({ value }: GridValueFormatterParams) =>
+        value ? formatDistanceToNowStrict((value as number) || 0) : "",
+    },
+  ];
+
+  if (isFallback) {
+    return <LinearProgress />;
+  }
+
   if (!Array.isArray(summonerNames)) {
     return null;
   }
 
+  if (!data) {
+    return <Typography>No data</Typography>;
+  }
+
   return (
-    <Container maxWidth="sm">
+    <Container maxWidth="xl">
       <Box my={4}>
         <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example {summonerNames}
+          Summoners
         </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
+        <Grid container >
+          {data.map(({ summoner, masteries }) => (
+            <Grid item style={{flexGrow: 1}} key={summoner.name}>
+              <Card>
+                <CardHeader
+                  title={summoner.name}
+                  subheader={`Total mastery ${masteries
+                    .reduce((prev, cur) => prev + cur.championPoints, 0)
+                    .toLocaleString()}`}
+                />
+                <CardContent>
+                  <div style={{ height: "500px", width: "100%" }}>
+                    <DataGrid
+                      rows={masteries.map((x) => ({ ...x, id: x.championId }))}
+                      columns={columns}
+                      autoPageSize
+                      disableSelectionOnClick
+                    />
+                  </div>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+        <Link href="/mastery" color="secondary">
+          Search
         </Link>
-        <ProTip />
         <Copyright />
       </Box>
     </Container>
@@ -35,12 +125,59 @@ const MasterySummonerNames = ({}: MasterySummonerNamesProps) => {
 
 export default MasterySummonerNames;
 
-export const getStaticProps: GetStaticProps<MasterySummonerNamesProps> = async (context) => {
+export const getStaticProps: GetStaticProps<MasterySummonerNamesProps> = async (
+  context
+) => {
+  let { summonerNames } = context.params;
+  if (!Array.isArray(summonerNames)) {
+    return {
+      props: {},
+      redirect: "/mastery",
+    };
+  }
+
+  // we only ever want to make 5 requests for summoner data
+  summonerNames = summonerNames.slice(0, 5);
+
+  const configuration = new Configuration({
+    apiKey: process.env.RG_API_KEY,
+    basePath: process.env.RG_API_BASE_URL,
+  });
+  const summonerApi = new SummonerV4Api(configuration);
+  const championApi = new ChampionMasteryV4Api(configuration);
+
+  const summonerApiCalls = await Promise.all(
+    summonerNames.map(async (summonerName) => {
+      const summonerResponse = await summonerApi.summonerV4GetBySummonerName(
+        summonerName
+      );
+
+      if (summonerResponse.statusText) {
+        console.log(summonerResponse.statusText);
+      }
+      if (summonerResponse.data) {
+        const masteryResponse =
+          await championApi.championMasteryV4GetAllChampionMasteries(
+            summonerResponse.data.id
+          );
+
+        if (masteryResponse.data) {
+          return {
+            summoner: summonerResponse.data,
+            masteries: masteryResponse.data,
+          };
+        }
+      }
+    })
+  );
+
   // ...
   return {
     props: {
-
+      data: summonerApiCalls,
     },
+    // seconds * minutes = 15mins
+    revalidate: 60 * 15,
   };
 };
 
@@ -59,7 +196,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
             "xherooftimex",
           ],
         },
-      }
+      },
     ],
     // Enable statically generating additional pages
     // For example: `/posts/3`
